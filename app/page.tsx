dfda37175ee75a206770a4e84592f894d2392bd5
Peter Dinis <peterdinis0@pobox.sk>
2024-09-12 18:19:18 +0200

    feat: Finish basic landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import { NextPage } from "next";
 import HomeWrapper from "./_components/home/HomeWrapper";
 import HeroServices from "./_components/home/HomeServices";
+import Footer from "./_components/shared/Footer";
+
+/* TODO: Update landing page later */
 
 const Homepage: NextPage = () => {
   return (
     <>
       <HomeWrapper />
       <HeroServices />
+      <Footer />
     </>
   )
 };
