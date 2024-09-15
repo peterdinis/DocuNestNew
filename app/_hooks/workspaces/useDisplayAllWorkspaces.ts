86a607ef8568dce53fb20301dcbde19840f36a0a
Peter Dinis <peterdinis0@pobox.sk>
2024-09-15 16:04:37 +0200

    FOO

---
+++ app/_hooks/workspaces/useDisplayAllWorkspaces.ts
@@ -0,0 +1,10 @@
+"use client"
+
+import { useQuery} from "@tanstack/react-query"
+import { fetchAllWorkspaces } from "@/app/_store/queries/workspaceQueries"
+
+const useDisplayAllWorkspaces = () => {
+    
+}
+
+export default useDisplayAllWorkspaces;
