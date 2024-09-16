4320c0d836a51e1858fbb9e1122144a9ea43fc50
Peter Dinis <peterdinis0@pobox.sk>
2024-09-16 21:06:14 +0200

    FOO

--- app/_components/workspaces/WorkspaceDetail.tsx
+++ app/_components/workspaces/WorkspaceDetail.tsx
@@ -1,7 +1,35 @@
+"use client"
+
 import { FC } from 'react';
+import GlobalLayout from '../shared/GlobalLayout';
+import Header from '../shared/Header';
+import { useParams } from 'next/navigation';
 
 const WorkspaceDetail: FC = () => {
-    return <>TODO: Later</>;
+    const {id} = useParams();
+    return (
+        <GlobalLayout>
+            <div className='flex-1 overflow-auto p-8'>
+                <div className='mx-auto max-w-4xl'>
+                    <Header text={`Workspace Detail`} />
+                    <br />
+                   <div className='mt-5'>
+                        DETAIL
+                        <div className='flex justify-end'>
+                            Button
+                        </div>
+                   </div>
+                   <div className='mt-5'>
+                        MEMBERS TABLE
+                   </div>
+
+                   <div className='mt-10'>
+                        DOCUMENTS TABLE
+                   </div>
+                </div>
+            </div>
+        </GlobalLayout>
+    );
 };
 
 export default WorkspaceDetail;
