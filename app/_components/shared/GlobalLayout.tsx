585f8a52ba52253ce869e33b3b0245de9cb46f33
Peter Dinis <peterdinis0@pobox.sk>
2024-09-16 13:05:28 +0200

    infra: Writing new helper layout

---
+++ app/_components/shared/GlobalLayout.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { FC, ReactNode } from 'react';
+import Sidebar from './Sidebar';
+
+interface IGlobalLayoutProps {
+    children?: ReactNode;
+}
+
+const GlobalLayout: FC<IGlobalLayoutProps> = ({ children }) => {
+    return (
+        <>
+            <div className='dark:bg-dark dark:text-light flex min-h-screen'>
+                <Sidebar />
+                <div className='flex-1 overflow-auto'>{children}</div>
+            </div>
+        </>
+    );
+};
+
+export default GlobalLayout;
