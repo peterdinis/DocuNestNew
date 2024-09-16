b061eb96efc192cc95bf785f6e5aea315719ed41
Peter Dinis <peterdinis0@pobox.sk>
2024-09-16 12:52:38 +0200

    feat: New hooks

--- app/_components/workspaces/WorkspacesLists.tsx
+++ app/_components/workspaces/WorkspacesLists.tsx
@@ -1,34 +1,22 @@
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { FC } from 'react';
+import CreateNewWorkspaceModal from './CreateNewWorkspaceModal';
 
 const WorkspacesLists: FC = () => {
     return (
         <>
             <Card className='mb-6 mt-4'>
                 <CardHeader>
-                    <CardTitle className='prose prose-h2: font-bold text-xl'>My all Workspaces</CardTitle>
+                    <CardTitle className='prose-h2: prose text-xl font-bold'>
+                        My all Workspaces
+                    </CardTitle>
                 </CardHeader>
+                <div className='float-right'>
+                    <CreateNewWorkspaceModal />
+                </div>
                 <CardContent>
                     <div className='grid grid-cols-3 gap-4 text-center'>
-                        <div>
-                            <p className='text-2xl font-bold'>12</p>
-                            <p className='text-muted-foreground'>
-                                Active Projects
-                            </p>
-                        </div>
-                        <div>
-                            <p className='text-2xl font-bold'>34</p>
-                            <p className='text-muted-foreground'>
-                                Team Members
-                            </p>
-                        </div>
-                        <div>
-                            <p className='text-2xl font-bold'>56</p>
-                            <p className='text-muted-foreground'>
-                                Tasks Completed
-                            </p>
-                        </div>
-                        
+                        Display data
                     </div>
                 </CardContent>
             </Card>
