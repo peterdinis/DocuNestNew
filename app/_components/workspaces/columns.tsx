93c05ab5b09f793f702a8007c65be17d460a8527
Peter Dinis <peterdinis0@pobox.sk>
2024-09-17 14:09:31 +0200

    FOO

--- app/_components/workspaces/columns.tsx
+++ app/_components/workspaces/columns.tsx
@@ -2,24 +2,51 @@
 
 import { ColumnDef } from '@tanstack/react-table';
 
-export type Payment = {
+export type Member = {
     id: string;
-    amount: number;
-    status: 'pending' | 'processing' | 'success' | 'failed';
+    name: string
     email: string;
+    role: string;
 };
 
-export const columns: ColumnDef<Payment>[] = [
+export type Document = {
+    id: number;
+    name: string;
+    createdAt: string | Date;
+    lastUpdate: string | Date;
+}
+
+export const documentColumns: ColumnDef<Document>[] = [
     {
-        accessorKey: 'status',
-        header: 'Status',
+        accessorKey: 'id',
+        header: 'Id',
+    },
+    {
+        accessorKey: 'name',
+        header: 'Name',
+    },
+    {
+        accessorKey: 'createdAt',
+        header: 'Created',
+    },
+
+    {
+        accessorKey: "lastUpdate",
+        header: "Last Update"
+    }
+]
+
+export const columns: ColumnDef<Member>[] = [
+    {
+        accessorKey: 'name',
+        header: 'Name',
     },
     {
         accessorKey: 'email',
         header: 'Email',
     },
     {
-        accessorKey: 'amount',
-        header: 'Amount',
+        accessorKey: 'role',
+        header: 'Role',
     },
 ];
