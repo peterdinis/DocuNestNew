baedf11e9d9652914e1a9fee9eb4dcc8e19fbc4a
Peter Dinis <peterdinis0@pobox.sk>
2024-09-19 13:51:50 +0200

    feat: Create new workspace document DONE

---
+++ app/_hooks/workspace-documents/useCreateWorkspaceDocument.ts
@@ -0,0 +1,33 @@
+"use client"
+
+import { newDocumentWorkspace } from "@/app/_store/mutations/workspaceDocumentMutations";
+import { WorkspaceDocumentType } from "@/app/_types/workspaceDocumentTypes";
+import { useToast } from "@/hooks/use-toast";
+import { useMutation } from "@tanstack/react-query"
+
+const useCreateWorkspaceDocument = () => {
+    const { toast } = useToast();
+    return useMutation({
+        mutationKey: ['newWorkspaceDocument'],
+        mutationFn: async (data: WorkspaceDocumentType) => {
+            return await newDocumentWorkspace(data);
+        },
+        onSuccess: () => {
+            toast({
+                title: 'New workspace document was created',
+                duration: 2000,
+                className: 'bg-green-800 text-white font-bold',
+            });
+        },
+
+        onError: () => {
+            toast({
+                title: 'New workspace document was not created',
+                duration: 2000,
+                className: 'bg-red-800 text-white font-bold',
+            });
+        },
+    });
+};
+
+export default useCreateWorkspaceDocument;
