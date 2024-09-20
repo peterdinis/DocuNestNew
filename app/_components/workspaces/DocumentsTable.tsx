228f3de2517d392ad45271ef65234e3aa003ccf1
Peter Dinis <peterdinis0@pobox.sk>
2024-09-20 16:06:30 +0200

    fix: Fixing bug with workspace documents

--- app/_components/workspaces/DocumentsTable.tsx
+++ app/_components/workspaces/DocumentsTable.tsx
@@ -8,23 +8,27 @@ interface IDocumentsTableProps {
     workspaceId: string;
 }
 
-const DocumentsTable: FC<IDocumentsTableProps> = ({workspaceId}: IDocumentsTableProps) => {
-    
-    const {data, isLoading, isError} = useAllWorkspaceDocuments({
-        id: workspaceId
+const DocumentsTable: FC<IDocumentsTableProps> = ({ workspaceId }) => {
+    const { data, isLoading, isError } = useAllWorkspaceDocuments({
+        id: workspaceId,
     });
 
-    console.log("D", data)
+    // Safely access data and ensure workspaceDocuments exists
+    const workspaceDocumentData = data?.[0]?.workspaceDocuments ?? [];
 
-    if(isLoading) return <Loader2 className='animate-spin w-8 h-8' />
-    
-    if(isError) return <p className='text-red-800 prose prose-p: font-bold text-xl'>Something went wrongs</p>
+    if (isLoading) return <Loader2 className="animate-spin w-8 h-8" />;
+
+    if (isError) return <p className="text-red-800 prose prose-p: font-bold text-xl">Something went wrong</p>;
 
     return (
         <>
-            <h4 className='prose-h4: prose ml-1 dark:text-sky-50'>Documents</h4>
-            <div className='mt-1'>
-                <GlobalTable data={data} columns={documentColumns} />
+            <h4 className="prose-h4: prose ml-1 dark:text-sky-50">Documents</h4>
+            <div className="mt-1">
+                {workspaceDocumentData.length > 0 ? (
+                    <GlobalTable data={workspaceDocumentData} columns={documentColumns} />
+                ) : (
+                    <p className="text-center">No documents available.</p>
+                )}
             </div>
         </>
     );
