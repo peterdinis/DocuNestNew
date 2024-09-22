0fb2fb0101f120ef57b8af87fafe915bf8c48369
Peter Dinis <peterdinis0@pobox.sk>
2024-09-22 11:15:50 +0200

    feat: start to design settings page WIP

---
+++ app/_components/settings/SettingsWrapper.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const SettingsWrapper: FC = () => {
+    return (
+        <>
+            WRAPPER
+        </>
+    )
+}
+
+export default SettingsWrapper;
---
+++ app/settings/page.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import SettingsWrapper from "../_components/settings/SettingsWrapper";
+
+const SettingsPage: NextPage = () => {
+    return <SettingsWrapper />
+}
+
+export default SettingsPage
