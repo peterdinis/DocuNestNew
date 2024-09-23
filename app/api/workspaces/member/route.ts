a5ec74ab742defc7ff0daa0f11103a28dafa634f
Peter Dinis <peterdinis0@pobox.sk>
2024-09-23 15:34:01 +0200

    FOO

--- app/api/workspaces/member/route.ts
+++ app/api/workspaces/member/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { db } from '@/app/_utils/db';
+import authOptions from '../../auth/authOptions';
 
 export async function GET(req: Request) {
-  const session = await getServerSession({ req });
-
+    const session = await getServerSession(authOptions);
   if (!session || !session.user) {
     return NextResponse.json({ error: 'Not authenticated' }, { status: 401 });
   }
