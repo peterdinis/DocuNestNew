244ba628a4cc2256e5737f65ff0446453063d20b
Peter Dinis <peterdinis0@pobox.sk>
2024-09-25 09:56:50 +0200

    refactor: Starat to refactoring Loader icons

---
+++ app/_components/shared/Loading.tsx
@@ -0,0 +1,8 @@
+import { Loader2 } from "lucide-react";
+import { FC } from "react";
+
+const Loading: FC = () => {
+    return <Loader2 className="animate-spin h-8 w-8" />
+}
+
+export default Loading;
