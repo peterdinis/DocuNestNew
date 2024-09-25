72d1b7d29375ad1371dae77e29babff95792a85c
Peter Dinis <peterdinis0@pobox.sk>
2024-09-25 10:58:27 +0200

    feat: Finish basic workspace member component

--- app/_components/workspaces/members/MemberWorkspaceLists.tsx
+++ app/_components/workspaces/members/MemberWorkspaceLists.tsx
@@ -5,6 +5,9 @@ import { FC } from 'react';
 import Loading from '../../shared/Loading';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
+import {motion} from "framer-motion";
+import Link from 'next/link';
+import { WorkspacePaginationType } from '@/app/_types/workspaceTypes';
 
 const MemberWorskapceLists: FC = () => {
     const {data, isLoading, isError} = useDisplayMyMemberWorkspaces();
@@ -18,8 +21,6 @@ const MemberWorskapceLists: FC = () => {
             </p>
         );
     }
-
-    console.log("Data", data);
 
     return (
         <>
@@ -38,46 +39,39 @@ const MemberWorskapceLists: FC = () => {
                 </CardHeader>
 
                 <CardContent>
-                    {/* <div className='grid grid-cols-3 gap-4'>
-                        {workspaces.length > 0 ? (
-                            workspaces.map(
-                                (workspace: WorkspacePaginationType) => (
-                                    <motion.div
-                                        key={workspace.id}
-                                        className='rounded-lg border bg-white p-4 shadow-md transition hover:shadow-lg dark:bg-zinc-800'
-                                        whileHover={{ scale: 1.05 }}
-                                        whileTap={{ scale: 0.95 }}
-                                    >
-                                        <Link
-                                            href={`/workspaces/${workspace.id}`}
-                                        >
-                                            <p className='text-2xl'>
-                                                {workspace.workspaceEmoji}
-                                            </p>
-                                            <p className='prose-p: prose text-lg font-bold dark:text-white'>
-                                                {workspace.name}
-                                            </p>
-                                            <p className='prose-p: prose text-sm text-gray-600 dark:text-white'>
-                                                {workspace.description ||
-                                                    'No description'}
-                                            </p>
-                                            <p className='text-xs text-gray-400 dark:text-white'>
-                                                Created at:{' '}
-                                                {new Date(
-                                                    workspace.createdAt,
-                                                ).toLocaleDateString()}
-                                            </p>
-                                        </Link>
-                                    </motion.div>
-                                ),
-                            )
-                        ) : (
-                            <p className='col-span-3 text-center'>
-                                No workspaces found.
-                            </p>
-                        )}
-                    </div> */}
-                </CardContent>
+                <div className='grid grid-cols-3 gap-4'>
+                    {data?.data.length > 0 ? (
+                        data?.data.map((workspace: WorkspacePaginationType) => (
+                            <motion.div
+                                key={workspace.id}
+                                className='rounded-lg border bg-white p-4 shadow-md transition hover:shadow-lg dark:bg-zinc-800'
+                                whileHover={{ scale: 1.05 }} // Zoom effect on hover
+                                whileTap={{ scale: 0.95 }}   // Scale down on tap
+                            >
+                                <Link href={`/workspaces/${workspace.id}`}>
+                                    <p className='text-2xl'>
+                                        {workspace.workspaceEmoji}
+                                    </p>
+                                    <p className='prose-p: prose text-lg font-bold dark:text-white'>
+                                        {workspace.name}
+                                    </p>
+                                    <p className='prose-p: prose text-sm text-gray-600 dark:text-white'>
+                                        {workspace.description || 'No description'}
+                                    </p>
+                                    <p className='text-xs text-gray-400 dark:text-white'>
+                                        Created at:{' '}
+                                        {new Date(workspace.createdAt).toLocaleDateString()}
+                                    </p>
+                                </Link>
+                            </motion.div>
+                        ))
+                    ) : (
+                        <p className='col-span-3 text-center'>
+                            No workspaces found.
+                        </p>
+                    )}
+                </div>
+            </CardContent>
             </Card>
         </>
     )
