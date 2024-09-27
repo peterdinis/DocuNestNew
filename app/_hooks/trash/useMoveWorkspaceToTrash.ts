297aea751102ca5102c2c5336ae3e5dca9dcdf12
Peter Dinis <peterdinis0@pobox.sk>
2024-09-27 16:19:06 +0200

    fix: Fixing trash bug

--- app/_hooks/trash/useMoveWorkspaceToTrash.ts
+++ app/_hooks/trash/useMoveWorkspaceToTrash.ts
@@ -4,6 +4,7 @@ import { moveWorkspaceToTrash } from '@/app/_store/mutations/trashMutations';
 import { queryClient } from '@/app/_store/queryClient';
 import { useToast } from '@/hooks/use-toast';
 import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 
 type WorkspaceProps = {
     id: string;
@@ -11,6 +12,8 @@ type WorkspaceProps = {
 
 const useMoveWorkspaceToTrash = ({ id }: WorkspaceProps) => {
     const { toast } = useToast();
+    const router = useRouter();
+
     return useMutation({
         mutationKey: ['moveToTrash', id],
         mutationFn: async () => {
@@ -22,6 +25,7 @@ const useMoveWorkspaceToTrash = ({ id }: WorkspaceProps) => {
                 duration: 2000,
                 className: 'bg-green-800 text-white font-bold',
             });
+            router.push("/workspaces");
             queryClient.invalidateQueries({
                 queryKey: ['trashWorkspaces'],
             });
