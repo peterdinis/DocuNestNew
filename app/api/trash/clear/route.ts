8165e6d24fb9fd6414a217cd17e303a0c50769d6
Peter Dinis <peterdinis0@pobox.sk>
2024-09-27 16:25:47 +0200

    feat: Working on clear trash logic

---
+++ app/api/trash/clear/route.ts
@@ -0,0 +1,26 @@
+import { getServerSession } from 'next-auth';
+import { NextResponse } from 'next/server';
+import { db } from '@/app/_utils/db';
+import authOptions from '../../auth/authOptions';
+
+export async function DELETE() {
+    const session = await getServerSession(authOptions);
+    if (!session || !session.user) {
+        return NextResponse.json(
+            { error: 'Not authenticated' },
+            { status: 401 },
+        );
+    }
+
+    const cleanTrash = await db.workspace.deleteMany({
+        where: {
+            userId: session.user.id,
+        },
+    });
+
+    console.log("CT", cleanTrash);
+
+    return NextResponse.json({
+        message: "Trash was cleaned"
+    });
+}
