6dba10f06269880ab71b5b05ed6f2efac8b6a95f
Peter Dinis <peterdinis0@pobox.sk>
2024-09-27 15:30:37 +0200

    feat: Trash hooks done :D

---
+++ app/_hooks/trash/useMoveWorkspaceToTrash.ts
@@ -0,0 +1,40 @@
+'use client';
+
+import { moveWorkspaceToTrash } from '@/app/_store/mutations/trashMutations';
+import { queryClient } from '@/app/_store/queryClient';
+import { useToast } from '@/hooks/use-toast';
+import { useMutation } from '@tanstack/react-query';
+
+type WorkspaceProps =  {
+    id: string;
+}
+
+const useMoveWorkspaceToTrash = ({id}: WorkspaceProps) => {
+    const { toast } = useToast();
+    return useMutation({
+        mutationKey: ['moveToTrash', id],
+        mutationFn: async () => {
+            return await moveWorkspaceToTrash(id);
+        },
+        onSuccess: () => {
+            toast({
+                title: 'Workspace was moved to trash',
+                duration: 2000,
+                className: 'bg-green-800 text-white font-bold',
+            });
+            queryClient.invalidateQueries({
+                queryKey: ['trashWorkspaces'],
+            });
+        },
+
+        onError: () => {
+            toast({
+                title: 'Workspace was not moved to trash',
+                duration: 2000,
+                className: 'bg-red-800 text-white font-bold',
+            });
+        },
+    });
+};
+
+export default useMoveWorkspaceToTrash;
---
+++ app/_store/mutations/trashMutations.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const moveWorkspaceToTrash = async (
+    id: string,
+) => {
+    return await axios.put(`/api/workspaces/${id}/trash`);
+};
