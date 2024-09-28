a10dfddac2e047d7fbb429fb065483b615376241
Peter Dinis <peterdinis0@pobox.sk>
2024-09-28 16:50:47 +0200

    FOO

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,6 +1,5 @@
 import { createRouteHandler } from 'uploadthing/next';
 import { uploadRouter } from './core';
-import { UTApi } from 'uploadthing/server';
 
 export const { GET, POST } = createRouteHandler({
     router: uploadRouter,
