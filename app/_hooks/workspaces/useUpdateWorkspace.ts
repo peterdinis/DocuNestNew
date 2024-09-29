442b9159908a9023c4fa5f32545b7f7fb6a29af5
Peter Dinis <peterdinis0@pobox.sk>
2024-09-29 12:47:16 +0200

    feat: New hook for updating workspace

---
+++ app/_hooks/workspaces/useUpdateWorkspace.ts
@@ -0,0 +1,41 @@
+'use client';
+
+import { useMutation } from '@tanstack/react-query';
+import { useToast } from '@/hooks/use-toast';
+import { UpdateWorkspaceType} from '@/app/_types/workspaceTypes';
+import { updateWorkspace } from '@/app/_store/mutations/workspaceMutations';
+import { queryClient } from '@/app/_store/queryClient';
+
+type Props = {
+    id: number;
+}
+
+const useUpdateWorkspace = ({id}: Props) => {
+    const { toast } = useToast();
+    return useMutation({
+        mutationKey: ['updateWorkspace', id],
+        mutationFn: async (data: UpdateWorkspaceType) => {
+            return await updateWorkspace(id, data);
+        },
+        onSuccess: () => {
+            toast({
+                title: 'Workspace was updated',
+                duration: 2000,
+                className: 'bg-green-800 text-white font-bold',
+            });
+            queryClient.invalidateQueries({
+                queryKey: ['myPaginatedWorkspaces'],
+            });
+        },
+
+        onError: () => {
+            toast({
+                title: 'Workspace was not updated',
+                duration: 2000,
+                className: 'bg-red-800 text-white font-bold',
+            });
+        },
+    });
+};
+
+export default useUpdateWorkspace;
