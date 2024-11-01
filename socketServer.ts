a2ce4794dc31547a32ed07d5f1ef1347f8ca8359
Peter Dinis <peterdinis0@pobox.sk>
2024-11-01 20:19:01 +0100

    FOO

--- socketServer.ts
+++ socketServer.ts
@@ -16,20 +16,6 @@ const io = new Server(server, {
     },
 });
 
-// Set up the connection event
-io.on('connection', (socket) => {
-    console.log('New client connected', socket.id);
-
-    socket.on('error', (error) => {
-        console.error('Socket error:', error);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Client disconnected', socket.id);
-    });
-});
-
-// Make the server listen on the defined port
 server.listen(PORT, () => {
     console.log(`Socket.IO server running on http://localhost:${PORT}`);
 });
