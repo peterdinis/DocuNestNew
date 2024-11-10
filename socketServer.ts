f19b662184c64568526ec004a3b09a4eed34d31e
Peter Dinis <peterdinis0@pobox.sk>
2024-11-10 13:00:48 +0100

    feat: Adding back old for socket.io

--- socketServer.ts
+++ socketServer.ts
@@ -1,10 +1,7 @@
 import { Server } from 'socket.io';
 import * as http from 'http';
-import * as dotenv from 'dotenv';
 
-dotenv.config();
-
-const PORT = process.env.SOCKET_SERVER_PORT;
+const PORT = 3001;
 
 const server = http.createServer();
 
@@ -19,14 +16,22 @@ const io = new Server(server, {
     },
 });
 
-server.listen(3001, () => {
-    io.on('connect', () => {
-        console.log('Connected to WebSocket server');
+// Set up the connection event
+io.on('connection', (socket) => {
+    console.log('New client connected', socket.id);
+
+    socket.on('error', (error) => {
+        console.error('Socket error:', error);
     });
-    io.on('connect_error', (err) => {
-        console.error('Connection error:', err);
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected', socket.id);
     });
-    console.log(`Socket.IO server running on http://localhost:${3001}`);
+});
+
+// Make the server listen on the defined port
+server.listen(PORT, () => {
+    console.log(`Socket.IO server running on http://localhost:${PORT}`);
 });
 
 export { server, io };
