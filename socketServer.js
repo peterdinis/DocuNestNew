a87f5f1f5ea7ad242f438c6909b837d427b90f0d
Peter Dinis <peterdinis0@pobox.sk>
2024-11-17 15:25:33 +0100

    FOO

--- socketServer.js
+++ socketServer.js
@@ -10,21 +10,26 @@ var io = new socket_io_1.Server(server, {
     cors: {
         origin: 'http://localhost:3000',
         methods: ['GET', 'POST'],
-        allowedHeaders: [
-            'Access-Control-Allow-Headers',
-            'Origin, X-Requested-With, Content-Type, Accept',
-        ],
+        allowedHeaders: ['Content-Type', 'Authorization'], // List of allowed headers
+        credentials: true, // If using cookies or Authorization headers
     },
 });
 exports.io = io;
 // Set up the connection event
 io.on('connection', function (socket) {
-    console.log('New client connected', socket.id);
+    console.log("New client connected: ".concat(socket.id));
+    // Handle custom events
+    socket.on('notification', function (data) {
+        console.log("Notification received:", data);
+        io.emit('notification', data); // Broadcast the notification to all clients
+    });
+    // Handle error events
     socket.on('error', function (error) {
         console.error('Socket error:', error);
     });
-    socket.on('disconnect', function () {
-        console.log('Client disconnected', socket.id);
+    // Handle client disconnection
+    socket.on('disconnect', function (reason) {
+        console.log("Client disconnected: ".concat(socket.id, " (Reason: ").concat(reason, ")"));
     });
 });
 // Make the server listen on the defined port
